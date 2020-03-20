3bcd1b719b3e5133b9e068aafdbc0e62966e1700
Lakshith Nishshanke <lakshith.knishshanke@gmail.com>
2020-03-20 13:17:07 +0530

    Update camlot.cpp

--- Training/usaco/Chapter 3/Section 3/camlot.cpp
+++ Training/usaco/Chapter 3/Section 3/camlot.cpp
@@ -25,6 +25,7 @@ int delta(int a,int b){
 }
 
 void BFS(int a,int b){
+  int Count = 0;
     bool visited[31][27];
     for(int i=0;i<=C;i++)
       for(int j=0;j<=R;j++){
@@ -41,20 +42,31 @@ void BFS(int a,int b){
        q.pop_front();
        int x,y;
        int r,c;
+       int Count = 0;
        for(x = -2;x<=2;x++)
         for(y = -2;y<=2;y++){
           if(x==0 || y==0)continue;
           if(x==y || x*-1==y)continue;
           int r = p.first +x;
           int c = p.second +y;
+          // Count ++;
           if(r<=R && c<=C && r>=1 && c>=1 && !visited[r][c]){
+            // cout << r << "<>" << c << endl;
             q.push_back({r,c});
             visited[r][c] = true;
             knightDis[a][b][r][c] = knightDis[a][b][p.first][p.second] + 1;
             cost[r][c] += knightDis[a][b][r][c];
+            Count ++;
           }
         }
+        // cout << Count << endl;
     }
+    cout << Count << endl;
+    // for(int i=1;i<=R;i++)
+    //   for(int j=1;j<=C;j++){
+    //     cout << i << "<>" << j << endl;
+    //     cout << visited[i][j]<< endl;
+    //   }
 }
 
 int main(){
@@ -68,18 +80,42 @@ int main(){
     in >> r;
     knights.push_back({r,c-'A'+1});
   }
+  for(int fr=1;fr<=R;fr++)
+    for(int fc=1;fc<=C;fc++)
+    for(int i=1;i<=R;i++)
+    for(int j=1;j<=C;j++)
+    knightDis[i][j][fr][fc] = INT_MAX;
+  // cout << r << c << endl;
+
   for(int i=1;i<=R;i++)
   for(int j=1;j<=C;j++)
     BFS(i,j);
-    int MIN = INT_MAX;
+
+    int MIN = INT_MAX-10;
     for(int fr=1;fr<=R;fr++)
       for(int fc=1;fc<=C;fc++){
         int D=0;
+        int z = 0;
+        int k = 0;
+        // cout << "+++++++++++++++++++=" << endl;
         for(pair<int,int> knight : knights){
+          // if(knightDis[knight.first][knight.second][fr][fc]==0){
+          //   z++;
+          //   if(z!=1){
+          //     D = INT_MAX;
+          //     break;
+          //   }
+          // }
+          k++;
           D += knightDis[knight.first][knight.second][fr][fc];
+          // cout << knightDis[knight.first][knight.second][fr][fc] << endl;
         }
+        // cout << D << endl;
         if(D>MIN)
           continue;
+
+        // cout << fr << " " << fc << endl;
+        // continue;
         for(int pr=1;pr<=R;pr++)
           for(int pc=1;pc<=C;pc++){
             int d;
@@ -87,10 +123,11 @@ int main(){
             int localMin = INT_MAX;
             for(int i=0;i<knights.size();i++){
               d = D - knightDis[knights.at(i).first][knights.at(i).second][fr][fc];
-              e = knightDis[knights.at(i).first][knights.at(i).second][pr][pc] + knightDis[pr][pc][fr][fc] + max(delta(pr,fr),delta(fc,pc));
+              e = knightDis[knights.at(i).first][knights.at(i).second][pr][pc] + knightDis[pr][pc][fr][fc] + max(abs(king.first-pr),abs(king.second-pc));
               localMin = min(localMin,d+e);
             }
-            if(knights.size()==0)localMin = max(delta(pr,fr),delta(fc,pc));
+            if(knights.size()==0)localMin = max(abs(king.first-pr),abs(king.second-pc));
+            if(localMin<0)continue;
             MIN = min(MIN,localMin);
           }
       }
